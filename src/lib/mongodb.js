c32477e1147da7041260ff4861cd231c5f944bec
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-08-22 13:48:06 -0500

    update mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -19,8 +19,8 @@ export async function mongodbConnect(collectionName) {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
         return client.db("productDB").collection(collectionName);
     } finally {
         // Ensures that the client will close when you finish/error
